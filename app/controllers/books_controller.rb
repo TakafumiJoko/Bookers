0738f4efd8045fd6a58035531777819a7635abdc
taka89 <jktkfm5535@gmail.com>
2020-05-16 13:26:35 +0000

    [Add] 投稿一覧ページ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,17 @@
 class BooksController < ApplicationController
+
   def top
   end
 
   def index
+    @book = Book.new
+    @books = Book.all
   end
 
   def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
   end
 
   def show
@@ -15,5 +21,11 @@ class BooksController < ApplicationController
   end
 
   def destroy
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
